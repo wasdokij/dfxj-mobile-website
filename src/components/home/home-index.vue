55e4d396afdca7591fd4dffef1425300bc714942
wasdokij <212259959@qq.com>
2017-01-18 18:04:14 +0800

    1

---
+++ src/components/home/home-index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="home-top">
+            <div class="bg-linear">
+                <div class="home-log-bg"></div>
+            </div>
+        </div>
+		<div class="ui-whitespace ui-justify-flex box-align">
+            <div class="box-align home-data">
+                <div class="xj-avatar-tiled margin-r-10">
+                    <span style="background-image:url(http://placeholder.qiniudn.com/60x60)"></span>
+                </div>
+                <div class="box-align">
+                    <div class="margin-r-5 font14 color-9b">这是标题，加ui-no</div>
+                    <div class="user-vip text-center font10">Lv2</div>
+                </div>
+            </div>
+			<div class="asset-btn">邀请好友</div>
+		</div>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+            }
+        },
+        components:{
+        },
+        methods: {
+        },
+    }
+</script>
