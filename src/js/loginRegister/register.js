1597a53d12e95784af61861adbf2677dd2a95e97
enjing <578951992@qq.com>
2017-01-20 18:12:26 +0800

    注册功能

--- src/js/loginRegister/register.js
+++ src/js/loginRegister/register.js
@@ -1,39 +1,101 @@
-//import Vue from 'vue';
-import axios from 'axios';
-import '../mock/test.js';
+//import '../mock/test.js';
 import '../lib/layer.js';
 import '../lib/layer.css';
+import { XHRPost } from '../ajax.js';
+import { countdown } from '../tools.js';
 
 var login = new Vue({
 	el: '#register',
 	data: {
-		name: '',
 		phone: '',
+		code: '',
 		pwd: '',
+		inviting_sn: '',
+		conut_time: 60,
+		getCodeBtnDisable: true,
 	},
-
+	
 	methods: {
-		goToRegister: function () {
-			let data = {
-				name: this.name,
-				phone: this.phone,
-				pws: this.pwd
+		getCode: function () {
+			const tel = /^1(3|4|5|7|8|9)\d{9}$/.test(this.phone);
+			if (!tel) {
+				this.errorTip('手机号码有误！');
+				return false;
 			}
-			console.log(data)
-			var load = layer.open({ type: 2,shadeClose: false})
-			axios.post('/api_login',data).then(function(response){
-				console.log(response)
-				layer.close(load);
-				if (response.data.status == 0) {
-					layer.open({
-						content: response.data.info,
-						btn: ['确定'],
-						yes: function () {
-							layer.closeAll();
+			this.getCodeBtnDisable = false;
+			XHRPost('/oriental_treasure/register_and_login/sendPhoneCode', this.phone, function (response) {
+				if (response.data.status === 1) {
+					const _this = this;
+					countdown(60,function (time) {
+						_this.conut_time = time;
+						if (time === 0) {
+							_this.getCodeBtnDisable = true;
 						}
 					});
+					this.errorTip(response.data.info);
+				} else {
+					this.errorTip(response.data.info);
+					this.getCodeBtnDisable = true;
+				}
+				
+			}.bind(this));
+			
+		},
+		goToRegister: function () {
+			var config = {
+				url: '/oriental_treasure/register_and_login/goRegister',
+//				url: '/api_login',
+				data: {
+					cellphone: encrypt(this.phone),
+					verify_code: '',
+					password: '',
+					inviting_sn: encrypt(this.inviting_sn),
+				},
+			}
+			
+			
+			const pwd = /^(\w){6,15}$/.test(this.pwd);
+			if (this.code) {
+				config.data.verify_code = encrypt(this.code);
+			} else {
+				this.errorTip('请输入验证码');
+				return false;
+			}
+			if (pwd) {
+				config.data.password = encrypt(this.pwd);
+			} else {
+				this.errorTip('密码有误，只能输入6-15个字母、数字、下划线');
+				return false;
+			}
+			
+			console.log(config.data)
+			var load = layer.open({ type: 2,shadeClose: false})
+			XHRPost(config.url, config.data, function (response) {
+				console.log(response)
+				layer.close(load);
+				if (response.data.status === 1) {
+					layer.open({
+						content: response.data.info,
+						time: 1.5,
+						end: function () {
+							window.location.href = '/xiaojin/index/index.html';
+						}
+					});
+
+				} else {
+					this.errorTip(response.data.info);
+				}
+			}.bind(this));
+		},
+		//错误提示方法
+		errorTip: function (msg) {
+			layer.open({
+				content: msg,
+				btn: ['确定'],
+				yes: function () {
+					layer.closeAll();
 				}
 			});
-		}
+		},
 	}
 });
