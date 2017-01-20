dbdb6812625400834bd13626670d8e009d0c423a
enjing <578951992@qq.com>
2017-01-20 10:40:50 +0800

    定义ajax get 请求方法

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -14,6 +14,7 @@ import axios from 'axios';
 //post请求
 export function XHRPost(url, data, callback, failBack) {
 	axios.post(url, data).then(function(response){
+		if (callback)
 		callback(response)
 	})
 	.catch(function (error) {
@@ -38,6 +39,29 @@ export function XHRPost(url, data, callback, failBack) {
 }
 
 //get请求
-export function XHRGet() {
-	
+export function XHRGet(url, data, callback, failBack) {
+	axios.get(url, data)
+  	.then(function (response) {
+    	if (callback)
+		callback(response)
+  	})
+  	.catch(function (error) {
+    	console.log(error);
+    	if (failBack) {
+			failBack(error);
+		} else {
+			if (window.layer) {
+		    	layer.closeAll();
+			    layer.open({
+			    	content: error,
+			    	btn: ['关闭'],
+					yes: function () {
+						layer.closeAll();
+					}
+			    });
+		    } else {
+		    	alert(error)
+		    }
+		}
+  	});
 }
