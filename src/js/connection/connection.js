f0cec334a452827b9d0c2cd9e899cb8a6f820e5c
HuangXingBin <356578436@qq.com>
2017-01-22 16:35:41 +0800

    更改了gulpfile.js 发布到svn更方便

--- src/js/connection/connection.js
+++ src/js/connection/connection.js
@@ -1,4 +1,7 @@
-// import Vue from 'vue';
+var dataUrl = '/oriental_treasure/Friend/mySubUser'; // page 大于0的整数
+var filterDataUrl = '/oriental_treasure/Friend/filterSubUsers'; // page, search: 昵称、手机号或姓名, type: 0[全部] 1[注册用户] 2[微股东] 3[银卡vip] 4[金卡vip]
+import Vue from 'vue';
+import Axios from 'axios';
 
 function bindFastClick() {
     // 启动fastclick
@@ -18,6 +21,15 @@ window.app = new Vue({
     },
     computed: {
 
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            Axios.post(dataUrl, {
+                page: 1
+            }).then(function (res) {
+                console.log('res', res);
+            }.bind(this))
+        });
     },
     directives: {
         focus: {
