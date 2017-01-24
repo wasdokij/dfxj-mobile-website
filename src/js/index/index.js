42b037393d69bcf95e7c481aed4b39d15950e7e6
wasdokij <212259959@qq.com>
2017-01-24 19:12:21 +0800

    回家做

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,4 +1,6 @@
 import IndexHome from 'components/index/index-home.vue';
+import '../lib/layer.js';
+import '../lib/layer.css';
 
 var V = new Vue({
     el: '#container',
--- src/js/index/invite.js
+++ src/js/index/invite.js
@@ -1,4 +1,6 @@
 import IndexInvite from 'components/index/index-invite.vue';
+import '../lib/layer.js';
+import '../lib/layer.css';
 
 var V = new Vue({
     el: '#container',
