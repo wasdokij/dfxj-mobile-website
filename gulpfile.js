71f85145fd1baec9290c68a63420db53ceae2ed7
enjing <578951992@qq.com>
2017-01-25 14:35:34 +0800

    fdsf

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ var webpackConfig = {
 	},
     externals: {
         'vue': 'Vue',
-        'axios': 'axios',
+//      'axios': 'axios',
         'vue-router': 'VueRouter',
         'vue-infinite-scroll': 'infiniteScroll'
     },
@@ -271,25 +271,25 @@ gulp.task('build', function () {
 	}));
 	build(function() {
 		del(['./src/tmp']);
-        cp('./public/**/*','/Users/gttx/Documents/jin-wechat/root/public/jin2.0/');
-		// cp('./public/**/*','../kongdian_api/public/jin2.0/');
+//      cp('./public/**/*','/Users/gttx/Documents/jin-wechat/root/public/jin2.0/');
+		 cp('./public/**/*','../kongdian_api/public/jin2.0/');
 
-		// cp('./public/views/*.html', '../kongdian_api/application/xiaojin/view/');
-        cp('./public/views/**/*.html', '/Users/gttx/Documents/jin-wechat/root/application/xiaojin/view/');
+		 cp('./public/views/*.html', '../kongdian_api/application/xiaojin/view/');
+//      cp('./public/views/**/*.html', '/Users/gttx/Documents/jin-wechat/root/application/xiaojin/view/');
 	});
 	// build的过程也要watch
     watch([src.js]).on('change', function () {
         // console.log('change', arguments);
         runSequence('js:build', 'ugjs:build', function () {
-            // cp('./public/**/*','../kongdian_api/public/jin2.0/');
-            cp('./public/**/*','/Users/gttx/Documents/jin-wechat/root/public/jin2.0/');
+               cp('./public/**/*','../kongdian_api/public/jin2.0/');
+//          cp('./public/**/*','/Users/gttx/Documents/jin-wechat/root/public/jin2.0/');
         })
     })
 
     watch([src.views]).on('change', function() {
 		runSequence('views:build', function () {
-            // cp('./public/views/**/*.html', '/Users/gttx/Documents/jin-wechat/root/application/xiaojin/view/');
-            cp('./public/views/**/*.html', '/Users/gttx/Documents/jin-wechat/root/application/xiaojin/view/');
+//          cp('./public/views/**/*.html', '/Users/gttx/Documents/jin-wechat/root/application/xiaojin/view/');
+            cp('./public/views/**/*.html', '../kongdian_api/application/xiaojin/view/');
         })
 	});
 
