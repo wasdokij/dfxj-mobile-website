8143e83c3d145f50090385176489f915046624d7
enjing <578951992@qq.com>
2017-01-25 14:48:01 +0800

    首页增加底部导航 ，修改底部导航组件

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,8 +1,9 @@
 import HomeIndex from 'components/index/home-index.vue';
+import jinFooter from 'components/common/footer.vue';
 
 var V = new Vue({
     el: '#container',
     components: {
-        HomeIndex,
+        HomeIndex, jinFooter
     },
 });
