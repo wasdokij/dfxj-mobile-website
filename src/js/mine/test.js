bd64d91cc90a7f6c72bc24dbd2efb3b647eb7147
enjing <578951992@qq.com>
2017-01-25 10:18:49 +0800

    删除底部导航的demo

--- src/js/mine/test.js
+++
@@ -1,9 +0,0 @@
-//import TipDialog from '../../components/common/tip-dialog.vue';
-import jinFooter from '../../components/common/footer.vue';
-new Vue({
-	el: '#app',
-	components: {
-//		TipDialog, 
-		jinFooter
-	}
-})
